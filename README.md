369f9b112a340d12ffad3ed1ee852fdfa1a96d43
the_loraxxz <the_loraxxz@web.de>
2022-01-28 16:27:29 +0100

    Feature added. Can now differentiate whether it is able to join the channel

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Before start
 - install ffmpeg on your server
-- 
+- get .env file from the_loraxxz
+
+# TODO:
+- Function that randomizes in an hour when the bot speaks randomly
+- On more people joining to say something
+- on movement in the team it follows 
+- checking whether he is able to join
+
+```
+channel = discord.utils.find(lambda c: c.name == 'some name' and c.type == 'voice', some_list_of_channels)
+if channel is not None:
+    print(channel.id)
+```
--- main.py
+++ main.py
@@ -31,35 +31,66 @@ async def on_ready():
 
 @client.event
 async def on_voice_state_update(member, before, after):
+    if member != client.user:  # to ignore actions by the bot
+        if after.channel is not None and before.channel is None:  # if someone is joining
 
-    if member != client.user:
-        if after.channel is not None and before.channel is None:
-            if len(after.channel.members) == 1:  # if the user is not the bot and someone connects to the channel it plays a sound
+            isInGuildConencted = False
+            voice = None
+
+            for voiceClient in client.voice_clients:
+                if voiceClient.channel.guild.id == after.channel.guild.id:
+                    voice = voiceClient
+                    isInGuildConencted = True
+
+            if isInGuildConencted:  # if it is already connected
+                if voice.channel.id == after.channel.id:  # if he connects to one where one is already in it
+                    time.sleep(5)
+                    speakrandom(voice)
+                else:
+                    time.sleep(1)
+                    permissions = after.channel.permissions_for(
+                        client.get_guild(after.channel.guild.id).get_member(client.user.id))
+                    if permissions.speak and permissions.connect:
+                        voiceClient = await after.channel.connect()
+                        speakrandom(voiceClient)
+                    else:
+                        print(f"cant connect to {after.channel}")
+
+            else:  # if it is new there it connects to it
                 time.sleep(1)
-                voiceclient = await after.channel.connect()  # wait x amount of seconds so it randomize joins
-                speakrandom(voiceclient)
+                permissions = after.channel.permissions_for(
+                    client.get_guild(after.channel.guild.id).get_member(client.user.id))
+                if permissions.speak and permissions.connect:
+                    voiceClient = await after.channel.connect()
+                    speakrandom(voiceClient)
+                else:
+                    print(f"cant connect to {after.channel}")
 
-
-
-        if after.channel is not None:
-            print(after.channel)
-
-        if before.channel is not None:
-            print(before.channel.members)
+        if before.channel is not None and after.channel is None:  # if someone is leaving
             if len(before.channel.members) == 1:
                 for voice_state in client.voice_clients:
                     if voice_state.channel.id == before.channel.id:
                         await voice_state.disconnect()
-                        print(voice_state.guild.id)
                         break
 
-
+        if before.channel is not None and after.channel is not None:
+            print(before)
 
 def speakrandom(voiceclient):
     time.sleep(2)  # wait 2 seconds so everybody is confest
     audio = audio_dir.get(random.randint(0, 9))  # get one of the audios (spezified in the audio dictionary)
     print(audio)
-    voiceclient.play(discord.FFmpegPCMAudio(dir_path + fr'\\audio\\{audio}.mp3'))  # play sound
+    voiceclient.play(discord.FFmpegPCMAudio(dir_path + fr'\audio\{audio}.mp3'))  # play sound
+
+
+def randomvoiceSpeak():
+    while (True):
+        if len(client.voice_clients) == 0:
+            break
+        else:
+            time.sleep(random.randint(0, 60))  # maximal eine stunde wo nichts passiert
+            for voice_client in client.voice_clients:
+                speakrandom(voice_client)
 
 
 client.run(os.getenv("DISCORD_TOKEN"))
