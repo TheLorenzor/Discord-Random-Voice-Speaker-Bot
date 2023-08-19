aeb45b432bf759bdcb8a1da917f55bcdcce42ec9
Tilmann <tilmann.lorenz@gmx.de>
2023-08-19 17:33:55 +0200

    more debugging

--- src/main.py
+++ src/main.py
@@ -15,6 +15,7 @@ audio_files_path =const.DATA_PATH / 'audio'
 audio_files = list(audio_files_path.glob('*.mp3'))
 @client.event
 async def on_ready():
+    print(f"{client.user} is Ready")
     for guild in client.guilds:
         for channel in guild.text_channels:
             user_member = guild.get_member(client.user.id)
